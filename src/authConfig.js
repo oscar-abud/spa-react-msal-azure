05f13bebbaaa5734d5832c44f8b23782cf1242d6
oscar-abud <oscarpalma.vpr2@gmail.com>
2026-09-10 16:06:19 -0300

    feat: update authentication flow and fix authority URL in authConfig

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,7 +1,7 @@
 export const msalConfig = {
   auth: {
     clientId: "b79b987c-d9bd-4b63-a995-40b4cff0113a", // Pega tu Client ID aquí
-    authority: "635fce4d-bfca-4393-994d-91819d9056e1", // Pega tu Tenant ID aquí
+    authority: "https://login.microsoftonline.com/635fce4d-bfca-4393-994d-91819d9056e1", // Pega tu Tenant ID aquí
     redirectUri: "http://localhost:5173",
   },
   cache: {
