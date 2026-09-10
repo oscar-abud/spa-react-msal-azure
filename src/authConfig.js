981e576e2de1e5939a4d86e072ccba93aad010f8
oscar-abud <oscarpalma.vpr2@gmail.com>
2026-09-10 16:00:30 -0300

    feat: integrate MSAL for authentication and create auth configuration

---
+++ src/authConfig.js
@@ -0,0 +1,15 @@
+export const msalConfig = {
+  auth: {
+    clientId: "b79b987c-d9bd-4b63-a995-40b4cff0113a", // Pega tu Client ID aquí
+    authority: "635fce4d-bfca-4393-994d-91819d9056e1", // Pega tu Tenant ID aquí
+    redirectUri: "http://localhost:5173",
+  },
+  cache: {
+    cacheLocation: "localStorage",
+    storeAuthStateInCookie: false,
+  },
+};
+
+export const loginRequest = {
+  scopes: ["User.Read"],
+};
